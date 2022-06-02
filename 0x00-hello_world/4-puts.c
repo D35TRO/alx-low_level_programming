77ed9d986f34fff4233548cf7e5c8901af09889d
D35TRO <akshetat2@gmail.com>
2022-06-02 15:03:59 -0700

    Task 4 Cp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
-/*
- * File: 4-puts.c
- * Auth: Akshay Pachkowdie <akshetat2@gmail.com>
- */
-
-#include <studio.h>
-
+#include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual 
- *                puzzle, followed by a new line."
  *
- * Return: Always 0.
+ * main - Entry block
+ * Return 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+	puts("\"Programming is like 
+			is like building a multilingual puzzle");
+	return (0)
 }
 
 
